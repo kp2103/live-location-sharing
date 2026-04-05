70719a1959e7ebbadc53f83aad8768aee7292302
krish-workplace <workplace.krish@gmail.com>
2026-04-05 10:48:32 +0530

    feat:implement the server

---
+++ server/index.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import {createServer} from 'http'
+import {Server} from 'socket.io'
+import cors from 'cors'
+
+const app = express()
+
+const server = createServer(app)
+const io = new Server(server,{
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+app.use(cors({
+    credentials : true,
+    origin : '*'
+}))
+
+// listen on io
+io.on('connection',(socket)=>{
+    console.log("new user is connected",socket.id)
+
+    // join in the same room 
+    socket.on('join-room',(roomId)=>{
+        socket.join(roomId);
+        console.log(`User ${socket.id} joined in the room ${roomId}`)
+    })
+
+    //listen on send location event 
+    socket.on('send-location',(data)=>{
+
+        const {lat,lng,roomId} = data
+
+        // emit the location
+        socket.to(roomId).emit('receive-location',{lat,lng})
+    })
+
+
+    socket.on('disconnect',()=>{
+        console.log("User Disconnected",socket.id)
+    })
+})
+
+
+server.listen(4000,()=>{
+    console.log("server is running at port number 4000")
+})
